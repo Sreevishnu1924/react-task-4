bcb7a14a9edf54d2b3f27a04e5a5c02439e4f030
Sreevishnu1924 <137443382+Sreevishnu1924@users.noreply.github.com>
2023-11-09 23:54:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ 
+import './App.css';
+import DataFetach from './components/DataFetach';
+ 
+
+function App() {
+  return (
+    <div >
+     
+       <DataFetach />
+    </div>
+  );
+}
+
+export default App;
